64f69bc4f53389839025fa923f667c7d0f205225
chrisdobra <104376667+chrisdobra@users.noreply.github.com>
2023-01-14 20:25:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-const userCardTemplate = document.querySelector("[data-user-template]")
-const userCardContainer = document.querySelector("[data-user-cards-container]")
-const searchInput = document.querySelector("[data-search]")
+const search = instantsearch({
+  appID: 'latency',
+  apiKey:'8e1b169fd2c844a93baaf7e4737f1d19',
+  indexName:'test',
+  urlSync:true
+});
 
-let users = []
-
-searchInput.addEventListener("input", e => {
-  const value = e.target.value.toLowerCase()
-  users.forEach(user => {
-    const isVisible =
-      user.name.toLowerCase().includes(value) ||
-      user.email.toLowerCase().includes(value)
-    user.element.classList.toggle("hide", !isVisible)
+search.addWidget(
+  instantsearch.widgets.searchBox({
+     container:'#search-input'
   })
-})
+);
 
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then(res => res.json())
-  .then(data => {
-    users = data.map(user => {
-      const card = userCardTemplate.content.cloneNode(true).children[0]
-      const header = card.querySelector("[data-header]")
-      const body = card.querySelector("[data-body]")
-      header.textContent = user.name
-      body.textContent = user.email
-      userCardContainer.append(card)
-      return { name: user.name, email: user.email, element: card }
-    })
+search.addWidget(
+  instantsearch.widgets.hits({
+     container:'#hits',
+     histsPerPage:10,
+     templates:{
+       item: document.getElementById('hit-template').innerHTML,
+       empty:"No results were found <em>\"{{query}}\"</em>"
+     }
   })
+);
 
+search.addWidget(
+   instantsearch.widgets.pagination({
+      container:'#pagination'
+   })
+);
+
+search.start();
